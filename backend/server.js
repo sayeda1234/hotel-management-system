2b838cd37562354f0cade51b262e441cf9717c0d
sayeda <you@example.com>
2026-04-15 14:59:44 +0530

    fixed cors for production

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://hotel-management-system.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
