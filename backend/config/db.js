6920745863fd5c0d5f25f00f50a4554a05405678
sayeda <you@example.com>
2026-04-15 15:20:11 +0530

    fix mongo atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/hotelDB', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected ✅');
-  } catch (error) {
-    console.error('MongoDB connection error ❌:', error.message);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected ✅");
+  } catch (err) {
+    console.error("MongoDB connection error ❌:", err.message);
     process.exit(1);
   }
 };
